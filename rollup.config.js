f993b37e771afa523ffe808013238c7322c78511
Jordy Morales <dev.morales.jordy@gmail.com>
2023-08-06 20:36:53 -0400

    feat: Improved SVG component configuration and TypeScript

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
+import typescript from '@rollup/plugin-typescript';
 import { babel } from '@rollup/plugin-babel';
 import filesize from 'rollup-plugin-filesize';
 import terser from '@rollup/plugin-terser';
 
 const config = {
-  input: 'src/index.js',
+  input: 'src/index.tsx',
   output: {
     file: 'dist/index.esm.js',
     format: 'esm',
   },
-  external: [/@babel\/runtime/, 'react'],
+  external: [/@babel\/runtime/, 'react', 'react-dom'],
   plugins: [
-    babel({ babelHelpers: 'runtime', plugins: ['@babel/plugin-transform-runtime'] }),
+    typescript(),
+    babel({
+      babelHelpers: 'runtime',
+      plugins: ['@babel/plugin-transform-runtime'],
+      extensions: ['.ts', '.tsx'],
+    }),
     filesize(),
     terser(),
   ],
